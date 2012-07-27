88fa00aa59185d00171b4d68043fab00e5bfe662
William Travis Holton <wtholton@googlemail.com>
2012-07-27 23:20:14 +1200

    added html mason views

---
+++ lib/GigBook/View/HTML.pm
@@ -0,0 +1,31 @@
+package GigBook::View::HTML;
+  use Moose;
+  extends 'Catalyst::View::HTML::Mason';
+
+  ## uncomment below to pass default configuration options to this view
+  # __PACKAGE__->config( );
+
+  =head1 NAME
+
+  GigBook::View::HTML - Mason View Component for GigBook
+
+  =head1 DESCRIPTION
+
+  Mason View Component for GigBook
+
+  =head1 SEE ALSO
+
+  L<GigBook>, L<Catalyst::View::HTML::Mason>, L<HTML::Mason>
+
+  =head1 AUTHOR
+
+  William Travis Holton
+
+  =head1 LICENSE
+
+  This library is free software . You can redistribute it and/or modify
+  it under the same terms as perl itself.
+
+  =cut
+
+  1;
---
+++ t/view_HTML.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+use Test::More;
+
+
+BEGIN { use_ok 'GigBook::View::HTML' }
+
+done_testing();
