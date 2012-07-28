8c7ade7880a558f08888b6cc3515931f409108d6
Jennie Yang <jennie@jennie-VGN-CR220E.(none)>
2012-07-28 13:27:27 +1200

    Fun stuff

--- root/static/scripts/gigbook.js
+++ root/static/scripts/gigbook.js
@@ -1,6 +1,6 @@
  $(document).ready(function() {
      $("#login").click(function(event) {
          $('#myModal').modal('show');
-         //alert("Thanks for visiting!");
+         event.preventDefault();
      });
  });
