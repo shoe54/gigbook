e1c66f059c772c06a705edce6b0415f4588bb7cf
Jennie Yang <jennie@jennie-VGN-CR220E.(none)>
2012-07-28 18:22:41 +1200

    more band photo popups

--- root/static/scripts/band.js
+++ root/static/scripts/band.js
@@ -1,9 +1,15 @@
  $(document).ready(function() {
 	 $('.carousel').carousel();
+     $('#am').popover({
+   	    content: $('#ampop').html()
+  	 });
+     $('#ch').popover({
+  	    content: $('#chpop').html()
+ 	 });
+     $('#dragon').popover({
+  	    content: $('#dragonpop').html()
+ 	 });
      $('#fotc').popover({
     	    content: $('#fotcpop').html()
-	 });
-     $('#ch').popover({
- 	    content: $('#chpop').html()
 	 });
  });
